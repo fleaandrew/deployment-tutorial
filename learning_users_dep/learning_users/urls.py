82d30ed61831d1a79a166339f520a1aa84406071
Andrew <andr_papusha@ukr.net>
2018-01-06 15:50:34 +0200

    Initial commit

---
+++ learning_users_dep/learning_users/urls.py
@@ -0,0 +1,28 @@
+"""learning_users URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from basic_app import views as basic_views
+
+
+urlpatterns = [
+    path('', basic_views.index, name='index'),
+    path('admin/', admin.site.urls),
+    path('basic/', include('basic_app.urls')),
+    path('login/', basic_views.user_login, name='login'),
+    path('logout/', basic_views.user_logout, name='logout'),
+    path('special/', basic_views.special, name='special'),
+]
